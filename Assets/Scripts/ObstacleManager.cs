5164189e549060bce623f72317c3e7d14d32066a
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2016-08-06 16:43:02 +0800

    Define new tag for obstacles called "Obstacle". Create ObstacleManager script, comment path forward with pseudo-code.

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObstacleManager : MonoBehaviour {
+	public float m_obstacleMovement;
+	public List<GameObject> m_obstacleObjects;
+	public bool m_started = true;
+
+	// Use this for initialization
+	void Start () {
+		m_obstacleObjects = new List<GameObject>(GameObject.FindGameObjectsWithTag("Obstacle"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// If game has started && !paused
+			// Update all game objects positions
+				// all game objects + m_obstacleMovement
+		// If trigger event on create collider
+			// Create new Obstacle prefab
+			// Add it game object array 
+		// If trigger event on destruction obstacle
+			// Detect object causing trigger and delete
+			// Remove from game object list
+	}
+}
