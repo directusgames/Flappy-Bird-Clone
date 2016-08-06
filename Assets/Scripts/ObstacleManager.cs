060e6f4df41fff9fc66ea0fa28ff20c29781c1d0
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2016-08-06 17:46:22 +0800

    Now moving the obstacles to dah left like god intended. Setting up collider behaviour.

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -3,9 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class ObstacleManager : MonoBehaviour {
-	public float m_obstacleMovement;
+	public float m_speed;
 	public List<GameObject> m_obstacleObjects;
 	public bool m_started = true;
+	public bool m_paused = false;
+
+	public BoxCollider2D m_createCollider;
+	public BoxCollider2D m_destroyCollider;
+
+	private Vector3 m_spawnOrigin = new Vector3 (400f, 0f, 0f);
 
 	// Use this for initialization
 	void Start () {
@@ -14,14 +20,27 @@ public class ObstacleManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// If game has started && !paused
-			// Update all game objects positions
-				// all game objects + m_obstacleMovement
+		if (m_started && !m_paused) {
+			foreach (GameObject obstacle in m_obstacleObjects) {
+				// Update all game objects positions
+				Vector3 curPos = obstacle.transform.position;
+				curPos.x -= m_speed * Time.deltaTime;
+				obstacle.transform.position = curPos;
+			}
+		}
 		// If trigger event on create collider
 			// Create new Obstacle prefab
 			// Add it game object array 
 		// If trigger event on destruction obstacle
 			// Detect object causing trigger and delete
 			// Remove from game object list
+	}
+
+	void destroyObstacle(GameObject obstacle) {
+
+	}
+
+	void createObstacle() {
+		// Create @ spawnOrigin.
 	}
 }
---
+++ Assets/Scripts/TriggerColliders.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerColliders : MonoBehaviour {
+	public ObstacleManager m_obstacleManager;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		switch (this.tag) {
+			case "createCollide":
+				break;
+		}
+		Debug.Log ("collision start");
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		switch (this.tag) {
+			case "destroyCollide":
+				break;
+		}
+		Debug.Log ("collision exit");
+	}
+}
