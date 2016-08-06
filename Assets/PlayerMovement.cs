c30f5907616639b7708884cb93a8bf7dff937654
directusgames <directusgames@gmail.com>
2016-08-06 17:45:29 +0800

    added jumping/gravity and rotation

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    public float jumpForce;
+    public float maxVelocityMag;
+    public float maxVelocity;
+    Rigidbody2D rigid;
+    
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+        if(Input.GetKeyDown (KeyCode.Space))
+        {
+           rigid.velocity = Vector2.zero;
+           rigid.AddForce(new Vector2(0f,1f) *  jumpForce);
+        }
+        
+        //Debug.Log (rigid.velocity);
+
+	}
+    
+    
+}
