3f0749ac107e8e374ccdc16fb61dfb5e2ea6c84e
directusgames <directusgames@gmail.com>
2016-08-24 16:44:49 +0800

    Added LowPoly rectangles

---
+++ Assets/ScrollBackground.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollBackground : MonoBehaviour {
+    
+    public float scrollSpeed;
+    public Renderer rend;
+    
+	// Use this for initialization
+	void Start () {
+        
+        rend = GetComponent<Renderer>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset,0));
+	}
+}
